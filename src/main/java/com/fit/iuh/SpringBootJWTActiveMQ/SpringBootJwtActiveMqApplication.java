626557e1366a6c2ccadf1963372ce8b275860248
hoanganh <hoanganh1410tb@gmail.com>
2022-10-07 17:35:14 +0700

    finish auth with spring boot + Jwt

---
+++ src/main/java/com/fit/iuh/SpringBootJWTActiveMQ/SpringBootJwtActiveMqApplication.java
@@ -0,0 +1,13 @@
+package com.fit.iuh.SpringBootJWTActiveMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtActiveMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtActiveMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fit/iuh/SpringBootJWTActiveMQ/SpringBootJwtActiveMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit.iuh.SpringBootJWTActiveMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtActiveMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
